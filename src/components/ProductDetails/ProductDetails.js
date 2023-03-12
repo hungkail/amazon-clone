8db6d8874c14c8ea5eb1d36ab35c6395ac812af9
Albert <aellrpeo@gmail.com>
2023-03-12 12:52:49 -0700

    Product Details Version 2 - Added Color to Text

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -25,12 +25,12 @@ function ProductDetails() {
         </div>
         <div className="productDetails__descriptionContainer">
             <h3>Optimum Nutrition Gold Standard 100% Whey Protein Powder, Extreme Milk Chocolate, 5 Pound (Packaging May Vary)</h3>
-            <span>Visit the Optimum Nutrition Store</span>
-            <span>⭐⭐⭐⭐⭐▼ 9,549 ratings | 143 answered questions</span>
-            <p>Price: $80.54 ($1.01 / Ounce)</p>
+            <span className="blueText">Visit the Optimum Nutrition Store</span>
+            <span className="blueText">⭐⭐⭐⭐⭐▼ 9,549 ratings | 143 answered questions</span>
+            <p className="redText">Price: $80.54 ($1.01 / Ounce)</p>
             <span><CheckCircleIcon />Save $8.29 at checkout Terms ▼</span>
-            <span>Coupon: <input type="checkbox" /> Save an extra 25% on your first Subscribe and Save Order.   Terms ▼</span>
-            <span>Get 5% back ($4.02 in rewards) on the amout charged to your Amazon Prime Rewards Visa Signature Card.</span>
+            <span className="greenText">Coupon: <input type="checkbox" /> Save an extra 25% on your first Subscribe and Save Order.   Terms ▼</span>
+            <span className="blueText">Get 5% back ($4.02 in rewards) on the amout charged to your Amazon Prime Rewards Visa Signature Card.</span>
             <span>May be available at a lower price from other sellers, potentially without free Prime Shipping.</span>
             <span>To use SNAP EBT, select one-time purchase</span>
             <div className="productDetails2">
@@ -55,21 +55,21 @@ function ProductDetails() {
                 <li>Gold Standard 100% Whey - 24g of protein per serving to help build and maintain muscle when taken ove time with regular resistance training</li>
                 <li>5.5g of naturally ocurring branched chain amino acids (BCAA and 11g of naturally occuring essential amino acids (EAAs) per serving to support muscle recovery</li>
                 <li>Anytime formula - great before or after exercise, between meals, with a meal, or any time of day when you need extra protein</li>
-                <li>THe world's best-selling whey protein powder</li>
+                <li>The world's best-selling whey protein powder</li>
                 <li>Banned substance tested and the highest quality control measures so you feel comfortable and confident consuming the product</li>
             </ul>
-            <span><CommentOutlinedIcon /> Report incorrect product information.</span>
+            <span className="blueText"><CommentOutlinedIcon /> Report incorrect product information.</span>
             <h3>Consider a similar item</h3>
             <div className="productDetails">
                 <div>
                     <img className="considerASimilarItem" src="https://m.media-amazon.com/images/I/71xqnuQ5JNL._AC_SY300_SX300_.jpg" alt="" />
                 </div>
                 <div>
-                    <span>Garden of Life Sport Whey Portein Powder Vanilla, Premium Grass Fed Whey Protein Isolate Plus Probiotics for Immune System Health, 24g Protein, Non GMO, Gluten Free, Cold Processed - 20 Servings</span>
-                    <span>1.41 Pound (Pack of 1)</span>
-                    <span>⭐⭐⭐⭐⭐ (3752)</span>
-                    <span>$41.44 ($1.84/Ounce)</span>
-                    <span>Climate Pledge Friendly</span>
+                    <span className="blueText">Garden of Life Sport Whey Portein Powder Vanilla, Premium Grass Fed Whey Protein Isolate Plus Probiotics for Immune System Health, 24g Protein, Non GMO, Gluten Free, Cold Processed - 20 Servings</span>
+                    <br/><span>1.41 Pound (Pack of 1)</span>
+                    <br/><span className="blueText">⭐⭐⭐⭐⭐ (3752)</span>
+                    <br/><span className="redText">$41.44 ($1.84/Ounce)</span>
+                    <br/><span>Climate Pledge Friendly</span>
                 </div>
             </div>
         </div>
